b0a364a911d60ac381aaf78577f6df1788e85b8b
Kilian Koeltzsch <me@kilian.io>
2017-02-01 14:36:15 +0100

    add api endpoint

---
+++ Sources/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  Pushover
+//
+//  Created by Kilian Költzsch on 01/02/2017.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoint {
+    static let apiBaseV1 = URL(string: "https://api.pushover.net/1/messages.json")!
+}
