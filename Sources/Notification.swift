ddc580bbb4e6b825953468e2607adbec16e03f40
Kilian Koeltzsch <me@kilian.io>
2017-02-01 17:16:29 +0100

    add sound enum

---
+++ Sources/Notification.swift
@@ -0,0 +1,9 @@
+//
+//  Notification.swift
+//  Pushover
+//
+//  Created by Kilian Költzsch on 01/02/2017.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Sound.swift
@@ -0,0 +1,34 @@
+//
+//  Sound.swift
+//  Pushover
+//
+//  Created by Kilian Költzsch on 01/02/2017.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+import Foundation
+
+public enum Sound: String {
+    case pushover
+    case bike
+    case bugle
+    case cashregister
+    case classical
+    case cosmic
+    case falling
+    case gamelan
+    case incoming
+    case intermission
+    case magic
+    case mechanical
+    case pianobar
+    case siren
+    case spacealarm
+    case tugboat
+    case alien
+    case climb
+    case persistent
+    case echo
+    case updown
+    case none
+}
