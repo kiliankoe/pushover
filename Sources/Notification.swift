a2b07f4a4827fb8b124fee697e7fb508e9ddda8b
Kilian Koeltzsch <me@kilian.io>
2017-02-01 17:17:03 +0100

    build out notification struct

--- Sources/Notification.swift
+++ Sources/Notification.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+public struct Notification {
+    let message: String
+    let title: String? = nil
+    let url: String? = nil
+    let urlTitle: String? = nil
+    let priority: Priority = .normal
+    let sound: Sound? = nil
+
+    var asParams: [String: String] {
+        var params = [String: String]()
+
+        params["message"] = message
+        if let title = title { params["title"] = title }
+        if let url = url { params["url"] = url }
+        if let urlTitle = urlTitle { params["url_title"] = urlTitle }
+        params["priority"] = "\(priority.rawValue)"
+        if let sound = sound { params["sound"] = sound.rawValue }
+
+        return params
+    }
+
+    var paramsString: String {
+        return asParams
+            .map { "\($0.key.urlEscaped)=\($0.value.urlEscaped)" }
+            .joined(separator: "&")
+    }
+}
+
+public enum Priority: Int {
+    case noAlert = -2
+    case quiet
+    case normal
+    case high
+    case requireConfirmation
+}
+
+extension URLRequest {
+    mutating func add(notification: Notification) {
+        self.httpBody = notification.paramsString.data(using: .utf8)
+    }
+}
+
+extension String {
+    var urlEscaped: String {
+        // this is sooo verbose >.<
+        return self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+    }
+}
