5a1480fb769e9c7a56a898fdc76062cab77212bb
Kilian Koeltzsch <me@kilian.io>
2017-02-02 16:39:24 +0100

    generic api handler

---
+++ Sources/API.swift
@@ -0,0 +1,30 @@
+//
+//  API.swift
+//  Pushover
+//
+//  Created by Kilian Költzsch on 02/02/2017.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+import Foundation
+
+typealias JSON = [String: Any]
+
+enum API {
+    static func send(_ request: URLRequest, onFailure fail: ((Error) -> Void)?, onSuccess succeed: ((Int, JSON) -> Void)?) {
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let _ = error { fail?(.network); return }
+
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { fail?(.network); return }
+
+            if case 500...599 = statusCode { fail?(.server); return }
+
+            guard let data = data else { fail?(.network); return }
+
+            guard let deserialized = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else { fail?(.decoding); return }
+            guard let json = deserialized as? JSON else { fail?(.decoding); return }
+
+            succeed?(statusCode, json)
+        }.resume()
+    }
+}
