5035ced946c87530b45ccc88f0fb0c7777c035a4
Kilian Koeltzsch <me@kilian.io>
2017-02-02 16:39:34 +0100

    add .decoding error case

--- Sources/Error.swift
+++ Sources/Error.swift
@@ -13,8 +13,10 @@ import Foundation
 /// - invalidRequest: The API didn't accept the request with a failure message.
 /// - server: Something unexpected went wrong on the server side.
 /// - network: Sending of the request failed.
+/// - decoding: The received data could not be decoded.
 public enum Error: Swift.Error {
     case invalidRequest(message: String)
     case server
     case network
+    case decoding
 }
