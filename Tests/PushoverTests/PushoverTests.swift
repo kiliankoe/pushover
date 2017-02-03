8ee3b490e7f995dd8baa82a4351d9127aab15a03
Kilian Koeltzsch <me@kilian.io>
2017-02-03 19:23:05 +0100

    add explicit notification test

--- Tests/PushoverTests/PushoverTests.swift
+++ Tests/PushoverTests/PushoverTests.swift
@@ -11,12 +11,34 @@ import XCTest
 import Pushover
 
 let EXAMPLE_TOKEN = "azGDORePK8gMaC0QOYAMyEEuzJnyUi"
+let EXAMPLE_USER = "poQWCJ812NgmER0QrTDF8D3Lnsd02L"
 
 class PushoverTests: XCTestCase {
     func testSimpleSend() {
         let e = expectation(description: "Send basic notification")
 
-        Pushover(token: EXAMPLE_TOKEN).send("Hello from Swift", to: "") { result in
+        Pushover(token: EXAMPLE_TOKEN).send("Hello from Swift", to: EXAMPLE_USER) { result in
+            e.fulfill()
+        }
+
+        waitForExpectations(timeout: 5) { error in
+            if let error = error {
+                XCTFail("\(error)")
+            }
+        }
+    }
+
+    func testAdvancedNotification() {
+        let e = expectation(description: "Send advanced notification")
+
+        var notification = Notification(message: "Hello from Swift", to: EXAMPLE_USER)
+        notification.title("foobar")
+        notification.devices(["iphone"])
+        notification.priority(.high)
+        notification.sound(.classical)
+        notification.timestamp(Date())
+
+        Pushover(token: EXAMPLE_TOKEN).send(notification) { result in
             e.fulfill()
         }
 
