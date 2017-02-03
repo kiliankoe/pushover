4bcac1ea90717503e90a5c05194c5473fde2841c
Kilian Koeltzsch <me@kilian.io>
2017-02-03 14:12:55 +0100

    Add Result

---
+++ Sources/Result.swift
@@ -0,0 +1,23 @@
+//
+//  Result.swift
+//  Pushover
+//
+//  Created by Kilian Költzsch on 03/02/2017.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+import Foundation
+
+public enum Result<T, Error: Swift.Error> {
+    case success(T)
+    case failure(Error)
+
+    public func dematerialize() throws -> T {
+        switch self {
+        case let .success(value):
+            return value
+        case let .failure(error):
+            throw error
+        }
+    }
+}
